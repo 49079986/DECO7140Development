c9affedd1a751be49b35a49b746815298eb5a05d
49079984 <s4907998@student.uq.edu.au>
2025-03-15 22:04:35 +1000

    javascript
    1

---
+++ work_phase_1/js/modules/logging.js
@@ -0,0 +1,9 @@
+/** Module: logging
+ * Generic longging using console.log
+ */
+
+function logMessage(pageName, message) {
+    console.log("from ${pageName}:${message}");
+}
+
+export { logMessage };
